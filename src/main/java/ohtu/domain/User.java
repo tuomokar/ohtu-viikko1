02e6b816e6f41a5b4ed8d6819952d136691a01a6
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-04-05 21:23:47 +0300

    init

---
+++ src/main/java/ohtu/domain/User.java
@@ -0,0 +1,23 @@
+package ohtu.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
